3c1216cd9e220b318e24539ef850e120e44a6adb
lubomirfilipov-byte <lubomir.filipov@icloud.com>
2025-10-26 01:48:10 +0300

    Add files via upload

---
+++ Zadacha1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Zadacha1
+    {
+        static void Main(string[] args)
+        {
+            int guessedNumber = 0;
+            Random number = new Random();
+            int randomNumber = number.Next(1, 10);
+                while (guessedNumber >= 1 || guessedNumber <= 10)
+            {
+                System.Console.WriteLine("Please enter a number between 1 and 10:");
+                guessedNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (guessedNumber > randomNumber)
+                {
+                    System.Console.WriteLine("Too high!");
+                }
+                else if (guessedNumber < randomNumber)
+                {
+                    System.Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    System.Console.WriteLine("Congratulations! You guessed the number!");
+                    break;
+                }
+            }
+               
+            
+        }
+    }
+}
